4611b30c6b81cbbd20b116c5e869bc2e667f050a
vosem <trefoil2@ukr.net>
2019-01-29 14:33:16 +0200

    added file-loader for png

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,27 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = function () {
-   return {
-       entry: './src/main.ts',
-       output: {
-           path: __dirname + '/dist',
-           filename: 'app.js'
-       },
-
-       plugins: [
-           new HtmlWebpackPlugin({
-               template: __dirname + '/src/index.html',
-               output: __dirname + '/dist',
-               inject: 'head'
-           })
-       ]
-   };
-}
+module.exports = {
+  entry: './src/main.ts',
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+    {
+        test: /\.(png|jpg)$/,
+        use: ['file-loader']
+    }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: `${__dirname}/src/index.html`,
+      output: `${__dirname}/dist`,
+      inject: 'head',
+    }),
+  ],
+  devServer: {
+    contentBase: "./dist"
+  }
+};
